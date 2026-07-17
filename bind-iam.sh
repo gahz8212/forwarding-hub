0cb8689593750040cb08d812b37f80abd8e37975
김성현 <berserk.gahz@gmail.com>
2026-07-17 14:32:44 +0900

    add script for iam binding

---
+++ bind-iam.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+SA="github-actions-deploy@forwarding-hub-502407.iam.gserviceaccount.com"
+PROJ="forwarding-ocr"
+ROLE="roles/iam.workloadIdentityUser"
+MEMBER="principalSet://iam.googleapis.com/181298989162/attribute.repository/gahz8212/forwarding-hub"
+
+CMD=(gcloud iam service-accounts add-iam-policy-binding "$SA")
+CMD+=(--project="$PROJ")
+CMD+=(--role="$ROLE")
+CMD+=(--member="$MEMBER")
+
+"${CMD[@]}"
+echo "✅ IAM Binding 완료!"
