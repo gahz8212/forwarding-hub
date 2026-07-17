665ef5999aa003bbebef5ef7ce3fd832bdcd0a71
김성현 <berserk.gahz@gmail.com>
2026-07-17 14:34:10 +0900

    update project id for iam binding

--- bind-iam.sh
+++ bind-iam.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SA="github-actions-deploy@forwarding-hub-502407.iam.gserviceaccount.com"
-PROJ="forwarding-ocr"
+PROJ="forwarding-hub-502407"
 ROLE="roles/iam.workloadIdentityUser"
 MEMBER="principalSet://iam.googleapis.com/181298989162/attribute.repository/gahz8212/forwarding-hub"
 
