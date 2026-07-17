05fa4d36a2649978e0a2ba3cd6dad9cdb2d5fae4
김성현 <berserk.gahz@gmail.com>
2026-07-17 14:35:24 +0900

    fix principalSet format for WIF IAM binding

--- bind-iam.sh
+++ bind-iam.sh
@@ -2,7 +2,7 @@
 SA="github-actions-deploy@forwarding-hub-502407.iam.gserviceaccount.com"
 PROJ="forwarding-hub-502407"
 ROLE="roles/iam.workloadIdentityUser"
-MEMBER="principalSet://iam.googleapis.com/181298989162/attribute.repository/gahz8212/forwarding-hub"
+MEMBER="principalSet://iam.googleapis.com/projects/181298989162/locations/global/workloadIdentityPools/github-actions-pool/attribute.repository/gahz8212/forwarding-hub"
 
 CMD=(gcloud iam service-accounts add-iam-policy-binding "$SA")
 CMD+=(--project="$PROJ")
